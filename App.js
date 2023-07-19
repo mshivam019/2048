855b2c7e2a95a2f8cd4dc39a699313d531cf5746
mshivam019 <mshivam019@gmail.com>
2023-07-19 19:48:55 +0530

    fix

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import 'react-native-reanimated';
 import GameBoard from './components/GameBoard';
 
 const App = () => {
